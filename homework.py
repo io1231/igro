e31c250df876b2b183383373a0af9396aea2c822
maslovbkm <maslovbkm@users.noreply.github.com>
2018-10-17 18:28:37 +0300

    Revert "Change md5 to sha256"
    This reverts commit bf485a658e6a7f4d095fbb723c5d4c48f5d48e54.

--- homework.py
+++ homework.py
@@ -1,8 +1,12 @@
 import hashlib
 
 my_name = "d.galamaga"
+<<<<<<< HEAD
 
 m = hashlib.sha256()
+=======
+m = hashlib.md5()
+>>>>>>> parent of bf485a6... Change md5 to sha256
 m.update(my_name.encode())
 
 if __name__ == "__main__":
