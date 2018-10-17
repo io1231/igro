18b648ca648e386e88005ce9253c4e3ca5216f80
maslovbkm <maslovbkm@users.noreply.github.com>
2018-10-17 18:37:29 +0300

    7

--- homework.py
+++ homework.py
@@ -1,17 +1,7 @@
 import hashlib
 
 my_name = "d.galamaga"
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-m = hashlib.sha256()
-=======
 m = hashlib.md5()
->>>>>>> parent of bf485a6... Change md5 to sha256
-=======
-m = hashlib.md5()
->>>>>>> parent of bf485a6... Change md5 to sha256
 m.update(my_name.encode())
 
 if __name__ == "__main__":
-    print(f"Task completed by {my_name}! md5 hash is {m.hexdigest()}")
